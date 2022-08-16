6c9f44088ac8154b5dbbe359855baedca8055ae5
ap-Camel <ayman.abdwlla@gmail.com>
2022-08-16 23:50:02 +0200

    fixed bug while editing an exam, it would edit all exams of the user

--- Dtos/ExamDtos/ExamUpdateDto.cs
+++ Dtos/ExamDtos/ExamUpdateDto.cs
@@ -1,5 +1,6 @@
 namespace QuizingApi.Dtos.ExamDtos {
     public record ExamUpdateDto {
+        public int ID {get; init;}
         public string title {get; init;}
         public int numOfQuestions {get; init;}
         public int duration {get; init;}
